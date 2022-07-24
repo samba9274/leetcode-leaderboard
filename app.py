5cc52163db4a013e6e67cb99021635b6e51e9d40
Yash Eksambekar <yasheksambekar@gmail.com>
2022-07-24 21:19:48 +0530

    Display name and rank

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 CORS(app)
 @app.route("/leaderboard", methods=["GET"])
 def leaderboard():
-    return "<pre>"+"*LeetCode Leaderboard*<br /><br />```Rank```   ```Username```<br />"+"<br />".join([f"    {index+1}       {user[0]} ({user[1][1]})" for index, user in enumerate(dict(sorted({x:(requests.get(f'https://leetcode-stats-api.herokuapp.com/{x}').json()['ranking'],request.args['names'].split(',')[int(i)]) for i,x in enumerate(request.args['users'].split(','))}.items(), key=lambda item: item[1][0])).items())])+"</pre>"
+    return "<pre>"+"*LeetCode Leaderboard*<br /><br />```Rank```   ```Username```<br />"+"<br />".join([f"    {index+1}       {user[1][1]} ({'{:,}'.format(user[1][0])})" for index, user in enumerate(dict(sorted({x:(requests.get(f'https://leetcode-stats-api.herokuapp.com/{x}').json()['ranking'],request.args['names'].split(',')[int(i)]) for i,x in enumerate(request.args['users'].split(','))}.items(), key=lambda item: item[1][0])).items())])+"</pre>"
     
 if __name__ == "__main__":
     app.run()
